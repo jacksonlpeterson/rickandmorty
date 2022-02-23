76a4b1912099d095a895d6cfd717db0244ab0e66
Jackson Peterson <jacksonlpeterson@gmail.com>
2022-02-22 22:49:20 -0700

    Need to fix search bar

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.getElementById("rick").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("morty").value;
+    if (value === "")
+      return;
+    console.log(value);
+fetch("https://rickandmortyapi.com/api/character")
+    .then(response=> response.json())
+    .then(data => makeCards(data.results));
+
+function makeCards(characters){
+    const cardContainer = document.querySelector("#card-group")
+    characters.forEach(character => {
+       
+        cardContainer.innerHTML = 
+        cardContainer.innerHTML +
+`
+    <div class = "card" id="#card-group" style="width: 18rem;">
+      <img src=${character.image} class = "card-img-top"</img>
+         <div class="card-body">
+            <h5 class="card-title">${character.name}</h5>
+            <h6>Status:${character.status}</h6>
+            <h6>Species:${character.species}</h6>
+            <h6>Gender:${character.gender}</h6>
+            <h6>Location:${character.location}</h6>
+        </div>   
+    </div>   
+      <br>`
+    })
+}
+
+
+
+})
